cba5489b4c69be76f0a7b345a887f6254b0fba67
Simon Hobbs <simon@hobbs.id.au>
2024-05-01 15:44:52 +1000

    Don't mind if there was a previous drupal.

--- core-dev/src/Command/InstallCommand.php
+++ core-dev/src/Command/InstallCommand.php
@@ -18,13 +18,19 @@ class InstallCommand extends CoreInstallCommand {
    */
   function execute(InputInterface $input, OutputInterface $output): int {
     $filesystem = new Filesystem();
-    $settings = __DIR__ . '/../../../../sites/default/settings.php';
-    $files = __DIR__ . '/../../../../sites/default/files';
-    $filesystem->chmod($files . '/../', 0755);
-    $filesystem->chmod($settings, 0777, 0000, true);
-    $filesystem->remove($settings);
-    $filesystem->chmod($files, 0777, 0000, true);
-    $filesystem->remove($files);
+    try {
+      $settings = __DIR__ . '/../../../../sites/default/settings.php';
+      $files = __DIR__ . '/../../../../sites/default/files';
+      $filesystem->chmod($files . '/../', 0755);
+      $filesystem->chmod($settings, 0777, 0000, true);
+      $filesystem->remove($settings);
+      $filesystem->chmod($files, 0777, 0000, true);
+      $filesystem->remove($files);
+      $output->writeln('Removed a previous installation.');
+    }
+    catch (\Exception $e) {
+      // This would fail if there was no previous installation, naively proceed to install Drupal.
+    }
     return parent::execute($input, $output);
   }
 }
