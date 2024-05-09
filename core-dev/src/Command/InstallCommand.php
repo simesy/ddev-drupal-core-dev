68c782b53326b5cc63f8294d90b94052edfce91b
Simon Hobbs <simon@hobbs.id.au>
2024-05-10 01:57:46 +1000

    Be a little more careful.

--- core-dev/src/Command/InstallCommand.php
+++ core-dev/src/Command/InstallCommand.php
@@ -18,20 +18,24 @@ class InstallCommand extends CoreInstallCommand {
    * {@inheritdoc}
    */
   function execute(InputInterface $input, OutputInterface $output): int {
+
+    // Minimally prepare Drupal for installation if it was previously installed.
     $filesystem = new Filesystem();
     $settings = __DIR__ . '/../../../../sites/default';
     $filesystem->chmod($settings, 0775, 0000, false);
-    try {
+    if ($filesystem->exists($settings . '/settings.php')) {
       $filesystem->remove($settings . '/settings.php');
-      $filesystem->remove($settings . '/files');
-      $output->writeln('Removed a previous installation.');
     }
-    catch (IOException $e) {
-      // This would fail if there was no previous installation, naively proceed to install Drupal.
+    if ($filesystem->exists($settings . '/files')) {
+      $filesystem->remove($settings . '/files');
     }
 
+    // Run the core installer.
     $return = parent::execute($input, $output);
+
+    // Keep settings directory readable for users who want to manually remove.
     $filesystem->chmod($settings, 0777, 0000, false);
+    $filesystem->chmod($settings . '/settings.php', 0775, 0000, false);
     return $return;
   }
 
